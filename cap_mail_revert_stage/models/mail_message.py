4bc0b433686e26ca6d8e572d94b4a75ee3630cc0
captivea-pgi <90042669+captivea-pgi@users.noreply.github.com>
2021-10-06 20:14:09 +0200

    test on check_variables

--- cap_mail_revert_stage/models/mail_message.py
+++ cap_mail_revert_stage/models/mail_message.py
@@ -26,7 +26,8 @@ class CapMailMessage(models.Model):
         if message.model in models_to_check and message.message_type == 'email':
             _logger.info("We are in the if")
             # check if all variables are True (we need these variable along the code)
-            # check_variables = all([message.author_id, message.author_id, message.res_id, message.model])
+            check_variables = all([message.author_id, message.author_id, message.res_id, message.model])
+            _logger.info("The check_variables is = %s" % check_variables)
             # if check_variables:
             
             #Link between the helpdesk.ticket model and the message's id
