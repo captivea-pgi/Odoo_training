b1e765c776d91ca86a5845c6f85355f1b4f4b66c
captivea-pgi <90042669+captivea-pgi@users.noreply.github.com>
2021-10-06 20:24:07 +0200

    Method fixed

--- cap_mail_revert_stage/models/mail_message.py
+++ cap_mail_revert_stage/models/mail_message.py
@@ -2,12 +2,12 @@
 
 from odoo import api, models, fields
 from odoo.exceptions import UserError
-import logging
+#import logging
 from odoo.tools.translate import _
 from datetime import datetime
 
 models_to_check = ['helpdesk.ticket']
-_logger = logging.getLogger(__name__)
+#_logger = logging.getLogger(__name__)
 
 class CapMailMessage(models.Model):
     _inherit = 'mail.message'
@@ -24,25 +24,16 @@ class CapMailMessage(models.Model):
         message = super(CapMailMessage, self).create(values)
         
         if message.model in models_to_check and message.message_type == 'email':
-            _logger.info("We are in the if")
-            # check if all variables are True (we need these variable along the code)
-            check_variables = all([message.author_id, message.author_id, message.res_id, message.model])
-            _logger.info("The check_variables is = %s" % check_variables)
-            # if check_variables:
-            
             #Link between the helpdesk.ticket model and the message's id
             linked_object = self.env[message.model].sudo().search([('id', '=', message.res_id)])
-            _logger.info("The link objet is = %s" % linked_object)
                 
             #check which team is linked to the ticket
             equipe = linked_object.team_id
-            _logger.info("The equipe is = %s" % equipe)
             
             #look for the min stage id for the group
             etape=min(self.env['helpdesk.stage'].sudo().search([('team_ids', 'in', equipe.id)]))
-            _logger.info("The etape is = %s" % etape)
 
-            #same as linked_object = stage_id
+            #same as linked_object.stage_id = etape.id
             linked_object.write({'stage_id': etape.id})
 
         return message
